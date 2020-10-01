1cd608af103a682b220178e3cac62cb0df72aae5
winstonbarbe <winston.barbe@gmail.com>
2020-10-01 10:55:24 -0400

    Adds encryped auth for API

---
+++ app/controllers/api/campsites_controller.rb
@@ -0,0 +1,7 @@
+class Api::CampsitesController < ApplicationController
+
+  def index
+    @campsites = HTTP.auth("Bearer #{Rails.application.credentials.campsites_api[:apikey]}").get("https://ridb.recreation.gov/api/v1/campsites?limit=50&offset=0").parse["RECDATA"]
+    render json: @campsites
+  end
+end
